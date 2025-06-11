f8a02b504f7837f65eac576efc0c8b0c4491f1df
Dalen Ainsworth <a.dalen@wustl.edu>
2025-06-11 09:35:56 -0500

    Testing deploy

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,4 @@
-// report.js
+// reportWebVitals.js
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
